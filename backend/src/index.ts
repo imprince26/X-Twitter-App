dcb83c53abc2a752e9ac66c3b6c6a7bcc42e704f
imprince26 <princepatel260806@gmail.com>
2025-07-05 14:29:20 +0530

    chore: update code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import user from './routes/user';
+// import user from './routes/user';
+import { connectDB } from './config/db';
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/user', user);
+connectDB();
+// app.use('/user', user);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
